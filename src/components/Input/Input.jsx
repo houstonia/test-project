4a9ed24d5bdc6d6674f4ddfbdc9c5ed83f948792
houstonia~ <uwuowo733@gmail.com>
2023-08-03 16:58:44 +0300

    add Input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,25 @@
+import { memo } from 'react';
+
+import styles from './Input.module.scss';
+
+const InputComponent = ({
+  name = '',
+  placeholder = '',
+  value = '',
+  onChange,
+  type = 'text',
+}) => {
+  return (
+    <div className={`${styles.input} ${type === 'number'? styles.input_num:''}`}>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export const Input =memo(InputComponent);
