78086ef3e7bf7589f39638175982e71003cf0a08
houstonia~ <uwuowo733@gmail.com>
2023-08-03 17:15:30 +0300

    add Main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import { Footer, Header } from '@components';
+import { Main } from '@pages';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter as Router, Outlet } from 'react-router-dom';
 
-const App=()=>{
-    return <div></div>
+
+function App() {
+  return (
+    <div className="app" style={{paddingBottom:'80px'}}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Outlet />
+                  <Footer />
+                </>
+              }
+            >
+              <Route index element={<Main />} />
+            </Route>
+          </Routes>
+        </Router>
+    </div>
+  );
 }
 export default App;
